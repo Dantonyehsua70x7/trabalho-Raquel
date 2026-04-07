c04b7d07286343f924ab0e6daa4108229f41a228
Dantonyehsua70x7 <danton.7406375@aluno.mg.gov.br>
2026-04-07 10:43:31 +0000

    Trabai

---
+++ script.js
@@ -0,0 +1,7 @@
+// Alerta de boas-vindas
+alert("Bem-vindo ao site sobre Tecnologia!");
+
+// Função de interação
+function mudarTexto() {
+  document.getElementById("mensagem").innerText = "A tecnologia facilita muito a nossa vida!";
+}
